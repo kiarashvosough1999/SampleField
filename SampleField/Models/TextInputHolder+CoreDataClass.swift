26166dea8dc435ec62124dd699ef8ad55e3d57b0
Kiarash Vosough <kiarash.vosough@divar.ir>
2022-02-10 00:28:17 +0330

    TextInputHolder model added to db

---
+++ SampleField/Models/TextInputHolder+CoreDataClass.swift
@@ -0,0 +1,44 @@
+//
+//  TextInputHolder+CoreDataClass.swift
+//  SampleField
+//
+//  Created by Kiarash Vosough on 2/9/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(TextInputHolder)
+public class TextInputHolder: NSManagedObject, ObjectConvertible {
+    
+    func from<T>(object: T) -> Bool where T : NSMangedObjectConvertible {
+        
+        if let object = object as? FirstInputHolderModel {
+            self.input = object.input
+            self.id = Int64(objectID.hashValue)
+            self.dateAdded = object.dateAdded
+            return true
+        }
+        
+        return false
+    }
+    
+    func toObject<T>() throws -> T? where T : NSMangedObjectConvertible {
+        guard let dateAdded = dateAdded,
+              let input = input else {
+                  throw FieldError.dbError(reason: .ValuesFoundNil(propertyNames: [
+                    #keyPath(dateAdded),
+                    #keyPath(input)
+                  ]))
+              }
+        
+        if T.self == FirstInputHolderModel.self {
+            return FirstInputHolderModel(identifier: identifier,
+                                         dateAdded: dateAdded,
+                                         input: input) as? T
+        }
+        
+        return nil
+    }
+}
