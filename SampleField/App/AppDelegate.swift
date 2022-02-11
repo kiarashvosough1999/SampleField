42065c43841410f5a23f7712002f3d86cfa5f15f
Kiarash Vosough <kiarash.vosough@divar.ir>
2022-02-11 13:16:14 +0330

    service registration moved to appDelegate

--- SampleField/App/AppDelegate.swift
+++ SampleField/App/AppDelegate.swift
@@ -13,7 +13,32 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        registerServices()
         return true
+    }
+    
+    fileprivate func registerServices() {
+        ServiceContainer.initial()
+        
+        ServiceContainer.main.addService { _ in
+            CoreDataPersistenceStorageAdapter()
+        }
+        .implements(PersistenceStoragePort.self)
+        .implements(PersistenceStorageInsertPort.self)
+        .implements(PersistenceStorageFetchPort.self)
+        .implements(PersistenceStorageDeletePort.self)
+        .implements(PersistenceStorageObserverPort.self)
+        .implements(PersistenceStorageUpdatePort.self)
+        
+        ServiceContainer.main.addService { res in
+            FirstInputServiceImpl(storagePort: res.getService()!)
+        }
+        .implements(FirstInputServicePort.self)
+        
+        ServiceContainer.main.addService { res in
+            FirstInputAdapter(firstInputServicePort: res.getService()!)
+        }
+        .implements(FirstInputAdapterDelegate.self)
     }
 
     // MARK: UISceneSession Lifecycle
